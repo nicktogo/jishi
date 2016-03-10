34edb892e2c393e976737c3f6ea8e141a7847c8b
kobpko <zhangjiaqi121@126.com>
2016-03-10 23:29:53 +0800

    message对应的四种操作， 1.  申请 2.  允许加入 3.  退出 4.  踢出 message 基本数据库操作 新建了 message 表 ，基本测试已通过

--- app/module/db/connection.py
+++ app/module/db/connection.py
@@ -39,10 +39,55 @@ class MysqlConnection(AbstractConnection):
         results = cursor.fetchall()
         return results
 
+    def insert_message(self, username, projectid, m_type):
+        cursor = self._conn_instance.cursor()
+        mes_num = self.find_mes_id(username, projectid, m_type)[0]
+        if mes_num == 0:
+            result = cursor.execute("insert into message(username, projectid, m_type)values('%s','%s','%s')"% (username, projectid, m_type))
+            self._conn_instance.commit()
+            return result
+        else:
+            print('you have already get one')
+
+    def find_mes_id(self, username, projectid, m_type):
+        cursor = self._conn_instance.cursor()
+        result = cursor.execute("select mes_id from message where username  = '%s'and projectid = '%s'" % (username, projectid))
+        if result == 0:
+            results = (result, -1)
+        else:
+            mes_id = cursor.fetchall()[0][0]
+            results = (result, mes_id)
+        return results
+
+    def merge_message(self,username, projectid, m_type):
+
+        query = """ UPDATE message
+                SET username = %s, projectid=%s, m_type= %s
+                WHERE mes_id = %s """
+        message_id = self.find_mes_id(username, projectid, m_type)[1]
+        data = (username, projectid, m_type, message_id)
+        cursor = self._conn_instance.cursor()
+        result = cursor.execute(query, data)
+        self._conn_instance.commit()
+        return result
+
+    def remove_message(self,username, projectid):
+        cursor = self._conn_instance.cursor()
+        mes_num = self.find_mes_id(username, projectid, 0)[0]
+        if mes_num == 1:
+            mes_id = self.find_mes_id(username, projectid, 0)[1]
+            result = cursor.execute("delete from message where mes_id = '%s'" % mes_id)
+            self._conn_instance.commit()
+            return result
+        else:
+            print('you do not have any message')
+
+
+
     #Belle
     def insert_user(self,username,password):
-        cursor=self._conn_instance.cursor()
-        result=cursor.execute("insert into user(username,password)values('%s','%s')"% (username,password))
+        cursor = self._conn_instance.cursor()
+        result = cursor.execute("insert into user(username,password)values('%s','%s')" % (username, password))
         self._conn_instance.commit()
         return result
 
@@ -81,4 +126,4 @@ class RedisConnection(AbstractConnection):
 
 if __name__ == '__main__':
     test = MysqlConnection()
-    print test.find_user_by_username('tzx')
+    print test.remove_message('liguanhua', '1', '0')
