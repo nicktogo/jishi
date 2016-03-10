0a0bd23e86d73e7b4d87a0a17b7d3f806598cd23
justnick <findnick@163.com>
2016-03-10 09:43:32 +0800

    more detail of project

--- app/module/forms.py
+++ app/module/forms.py
@@ -1,10 +1,14 @@
 # coding=utf-8
 from flask.ext.wtf import Form
-from wtforms import TextAreaField, StringField, SubmitField
+from wtforms import TextAreaField, StringField, SelectField, SubmitField
 from wtforms.validators import DataRequired
 
 
 class ProjectForm(Form):
     title = StringField(u"项目名称", validators=[DataRequired()])
     describe = TextAreaField(u"项目描述", validators=[DataRequired()])
+    requirements = TextAreaField(u"项目需求", validators=[DataRequired()])
+    number_of_member = SelectField(u"项目人数", validators=[DataRequired()], coerce=int,
+                                   choices=[(1, 1), (2, 2), (3, 3)])
+    time = StringField(u"时间", validators=[DataRequired()])
     submit = SubmitField(u"提交")
