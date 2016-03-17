f92c2626a33586dc36fbf0f759096cabdb39f940
hxx <793777976@qq.com>
2016-03-17 08:40:51 +0800

    form style

--- app/module/forms.py
+++ app/module/forms.py
@@ -6,7 +6,7 @@ from wtforms.validators import DataRequired
 
 class ProjectForm(Form):
     title = StringField(u"项目名称", validators=[DataRequired()])
-    describe = TextAreaField(u"项目描述", validators=[DataRequired()])
+    des = TextAreaField(u"项目描述", validators=[DataRequired()])
     requirements = TextAreaField(u"项目需求", validators=[DataRequired()])
     number_of_member = SelectField(u"项目人数", validators=[DataRequired()], coerce=int,
                                    choices=[(1, 1), (2, 2), (3, 3)])
