92165a4f14e366a3252f650924cae9ad1a5f4435
谭靖儒 <tanjingru@tanjingrudeMacBook-Pro.local>
2016-05-04 22:37:05 +0800

    weibo

--- app/app.py
+++ app/app.py
@@ -49,7 +49,7 @@ def get_code():
     print 'access_token is', access_token
     expires_in = r.expires_in
     client_.set_access_token(access_token, expires_in)
-    return '123'
+    return redirect(url_for(index))
 
 
 @app.route('/auth/homepage', methods=['POST', 'GET'])
