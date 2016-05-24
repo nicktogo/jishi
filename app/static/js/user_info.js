13adbb39668ef1bab6f8521329648d148349507c
justnick <findnick@163.com>
2016-05-24 11:32:12 +0800

    个人信息 bootstrap jquery 。。。换成cdn

---
+++ app/static/js/user_info.js
@@ -0,0 +1,126 @@
+/**
+ * Created by Kupletsky Sergey on 17.10.14.
+ *
+ * Material Sidebar (Profile menu)
+ * Tested on Win8.1 with browsers: Chrome 37, Firefox 32, Opera 25, IE 11, Safari 5.1.7
+ * You can use this sidebar in Bootstrap (v3) projects. HTML-markup like Navbar bootstrap component will make your work easier.
+ * Dropdown menu and sidebar toggle button works with JQuery and Bootstrap.min.js
+ */
+
+// Sidebar toggle
+//
+// -------------------
+$(document).ready(function() {
+    var overlay = $('.sidebar-overlay');
+
+    overlay.on('click', function() {
+        $(this).removeClass('active');
+        $('#sidebar').removeClass('open');
+    });
+
+});
+
+// Sidebar constructor
+//
+// -------------------
+$(document).ready(function() {
+
+    var sidebar = $('#sidebar');
+
+    // Sidebar position
+    $('#sidebar-position').change(function() {
+        var value = $( this ).val();
+        sidebar.removeClass('sidebar-fixed-left sidebar-fixed-right sidebar-stacked').addClass(value).addClass('open');
+        if (value == 'sidebar-fixed-left' || value == 'sidebar-fixed-right') {
+            $('.sidebar-overlay').addClass('active');
+        }
+    });
+
+    // Sidebar theme
+    $('#sidebar-theme').change(function() {
+        var value = $( this ).val();
+        sidebar.removeClass('sidebar-default sidebar-inverse sidebar-colored sidebar-colored-inverse').addClass(value)
+    });
+});
+
+/**
+ * Created by Kupletsky Sergey on 08.09.14.
+ *
+ * Add JQuery animation to bootstrap dropdown elements.
+ */
+
+(function($) {
+    var dropdown = $('.dropdown');
+
+    // Add slidedown animation to dropdown
+    dropdown.on('show.bs.dropdown', function(e){
+        $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+    });
+
+    // Add slideup animation to dropdown
+    dropdown.on('hide.bs.dropdown', function(e){
+        $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+    });
+})(jQuery);
+
+
+
+(function(removeClass) {
+
+	jQuery.fn.removeClass = function( value ) {
+		if ( value && typeof value.test === "function" ) {
+			for ( var i = 0, l = this.length; i < l; i++ ) {
+				var elem = this[i];
+				if ( elem.nodeType === 1 && elem.className ) {
+					var classNames = elem.className.split( /\s+/ );
+
+					for ( var n = classNames.length; n--; ) {
+						if ( value.test(classNames[n]) ) {
+							classNames.splice(n, 1);
+						}
+					}
+					elem.className = jQuery.trim( classNames.join(" ") );
+				}
+			}
+		} else {
+			removeClass.call(this, value);
+		}
+		return this;
+	}
+
+})(jQuery.fn.removeClass);
+
+$(document).ready(function() {
+    var panels = $('.user-infos');
+    var panelsButton = $('.dropdown-user');
+    panels.hide();
+
+    //Click dropdown
+    panelsButton.click(function() {
+        //get data-for attribute
+        var dataFor = $(this).attr('data-for');
+        var idFor = $(dataFor);
+
+        //current button
+        var currentButton = $(this);
+        idFor.slideToggle(400, function() {
+            //Completed slidetoggle
+            if(idFor.is(':visible'))
+            {
+                currentButton.html('<i class="glyphicon glyphicon-chevron-up text-muted"></i>');
+            }
+            else
+            {
+                currentButton.html('<i class="glyphicon glyphicon-chevron-down text-muted"></i>');
+            }
+        })
+    });
+
+
+    $('[data-toggle="tooltip"]').tooltip();
+
+    $('button').click(function(e) {
+        e.preventDefault();
+        alert("This is a demo.\n :-)");
+    });
+});
