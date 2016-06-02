6bfa090e08562db83db413a6c4e545e2b33527ce
谭靖儒 <tanjingru@tz.local>
2016-06-02 10:57:08 +0800

    接口

--- app/app.py
+++ app/app.py
@@ -61,7 +61,7 @@ def get_code():
     client_.set_access_token(access_token, expires_in)
     g.client = client_
     g.uid = r.uid
-    session['weibo'] =  g.client.users.show(uid=g.uid)
+    session['weibo'] =  g.client.users.show.get(uid=g.uid)
     session['username'] = session['weibo']['name']
     return redirect(url_for('index'))
 
