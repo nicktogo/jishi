c882d0c45e874daa942af97dc0d07c811782ee14
谭靖儒 <tanjingru@tz.local>
2016-06-02 13:12:50 +0800

    ...

--- app/app.py
+++ app/app.py
@@ -69,17 +69,14 @@ def get_code():
 
 @app.route('/project/share', methods=['POST'])
 def share_project():
-    print '123'
     j = request.json
     module_dir = os.path.dirname(__file__)
     f_path = os.path.join(module_dir, 'static', 'img', 'logo.png')
     f = open(f_path, 'rb')
     client_ = APIClient(app_key=APP_KEY, app_secret=APP_SECRET, redirect_uri=CALLBACK_URL)
     client_.set_access_token(session['access_token'], session['expires_in'])
-    client_.statuses.update.post(status=u'项目分享!! 项目地址: http://tztztztztz.org:5000/project/'+j['project_id'], pic=f)
+    client_.statuses.upload.post(status=u'济事项目分享,小伙伴们快来加入吧 http://tztztztztz.org:5000/project/'+j['project_id'], pic=f)
     return jsonify(dict(a=123))
-
-
 
 
 @app.route('/auth/homepage', methods=['POST', 'GET'])
